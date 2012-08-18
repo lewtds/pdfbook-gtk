6c8eab19d3e34780dc80ab25bb33d2091b420023
Ngô Trung <ndtrung4419@gmail.com>
2012-08-18 14:17:14 +0700

    Select a generic themed icon

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 files = ["data/ui.glade"]
 
 setup(name='pdfbook-gtk',
-      version='1.0',
+      version='1.0.1',
       packages=['pdfbookgtk'],
       package_data={'pdfbookgtk':files},
       data_files=[("share/applications", ["pdfbook-gtk.desktop"])],
